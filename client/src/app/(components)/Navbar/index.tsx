3a42e083c097cacd717044008c56d2ebfad659cf
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-09-02 23:35:21 +0530

    feat: update Tailwind CSS configuration and add new dependencies

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import {Search} from "lucide-react"
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
-    <div className='flex item-center justify-center bg-white px-4 py-3 dark:bg-black dark:px-4 dark:py-3'>
+  <div className="flex items-center justify-between bg-white px-4 py-3">
       {/* {Search bar} */}
-    <div className='flex item-center gap-8'>
-      <div className=''>
-         
+    <div className="flex item-center gap-8">
+      <div className="relative flex h-min w-[200px]">
+         <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer" />
       </div>
     </div>
     </div>
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "@/app/(components)/Navbar"
 const dashboardWrapper = ({children} :{children: React.ReactNode}) => {
   return (
-    <div className='flex min-h-screen w-full bg-grey-50 text-gray-100'>
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
       {/* Sidebar */}
       sidebar
       <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
